c71b380564e7e89d5cf5a36d8f7ed3363d840e7a
Nadzrien <jerinho@users.noreply.github.com>
2025-05-28 13:49:27 +0800

    + push listener

--- sw.js
+++ sw.js
@@ -7,3 +7,14 @@ self.addEventListener('notificationclick', function(event) {
   event.notification.close();
   event.waitUntil(clients.openWindow('/'));
 });
+self.addEventListener('push', function(event) {
+  const data = event.data.json();
+  const title = data.notification.title;
+  const options = {
+    body: data.notification.body,
+    icon: 'logo.png',
+  };
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
+});
